18b99b91b0d414d8217706034b23e2d769bfe402
yukii <lichtestv@gmail.com>
2025-08-08 13:39:23 +0200

    dynamicing my array

--- src/darray.h
+++ src/darray.h
@@ -1,6 +1,7 @@
 #ifndef DARRY_H
 #define DARRY_H
 
+#include <assert.h>
 #include <stddef.h>
 
 #define USING_ARENA
@@ -12,6 +13,7 @@ typedef struct {
     void* items;
     size_t count;
     size_t capacity;
+    size_t element_size;
     Arena* arena; 
 } DArray;
 
@@ -20,6 +22,7 @@ static inline DArray _darray_new_impl(Arena* arena, size_t element_size, size_t 
         .items = capacity > 0 ? arena_alloc(arena, element_size * capacity) : NULL,
         .count = 0,
         .capacity = capacity,
+        .element_size = element_size,
         .arena = arena,
     };
 }
@@ -33,4 +36,30 @@ static inline DArray _darray_new_impl(Arena* arena, size_t element_size, size_t 
 #define darray_new_empty(arena, type) \
     _darray_new_impl(arena, sizeof(type), 0)
 
+static inline void _darray_push(DArray* da, size_t size, void* item) {
+    assert(da->element_size == size);
+
+    if (da -> count >= da -> capacity) {
+        size_t old_size = da -> capacity * da -> element_size;
+        size_t new_size = (da -> capacity ? da -> capacity * 2 : 4) * da -> element_size;
+
+        da -> items = arena_realloc(da -> arena, da -> items, old_size, new_size);
+        da -> capacity = (da -> capacity ? da -> capacity * 2 : 4);
+    }
+
+    arena_memcpy((char*)da -> items + (size * da -> count), item, size);
+    da -> count++;
+}
+
+#define darray_push(da, type, item) \
+    _darray_push(da, sizeof(type), &(item))
+
+static inline void* _darray_get(DArray* da, size_t index) {
+    if (index >= da -> count) return NULL;
+    return (void*) ((char*) da -> items + (index * da -> element_size));
+}
+
+#define darray_get(da, index) \
+    _darray_get(da, index)
+ 
 #endif // !DARRY_H
--- test/test.c
+++ test/test.c
@@ -1,9 +1,21 @@
 #include "../src/darray.h"
 #include "../src/arena.h"
 
+#include <stdio.h>
+
 static Arena arena = {0};
 
 int main() {
     DArray arr = darray_new(&arena, int);
 
+    int a = 1;
+    int b = 2;
+    darray_push(&arr, int, a);
+    darray_push(&arr, int, b);
+    
+    for (int i = 0; i < arr.count; i++) {
+        printf("%d\n", *(int*) darray_get(&arr, i));
+    }
+
+    arena_free(&arena);
 }
